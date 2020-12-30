a8be0792566fa98ac62633282c4f82e098615f21
chest <marketpetr98@gmail.com>
2020-12-30 22:43:09 +0300

    ver 1

---
+++ README.md
@@ -0,0 +1,13 @@
+# new-year-greetings-via-vk.com
+
+This script sends all your friends in a vk.com the text you need. In this case, the script is intended for sending Happy New Year greetings.
+
+1. Get vk api token. To be able to send messages, create a token. Needs VK Admin.
+[https://vkhost.github.io/][https://vkhost.github.io/]
+
+2. Insert token and message text into `main.py` file .
+
+4. Run python code `python main.py`
+
+
+[https://vkhost.github.io/]: https://vkhost.github.io/
---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+import requests
+
+
+token = 'your token'
+message_text = 'Happy New Year 🎅🎁🎉🎄🌟🍾🥂✨'
+
+friends = requests.request('GET', f"https://api.vk.com/method/friends.get?access_token={token}&v=5.21").json()['response']['items']
+for friend in friends:
+    try:
+        url = f"https://api.vk.com/method/messages.send?access_token={token}&v=5.21&user_id={friend}&message={message_text}"
+        requests.request('GET', url)
+    except:
+        pass
+    time.sleep(1)
